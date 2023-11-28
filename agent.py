e12b987e4c3c38e6b0e40928f0689068ef35dd2a
khashayarghamati <khashayarghamati@gmail.com>
2023-11-28 23:15:03 +0000

    use conv layers

--- agent.py
+++ agent.py
@@ -28,7 +28,7 @@ class Agent:
         self.save_dir = save_dir
 
         self.memory = deque(maxlen=100000)
-        self.batch_size = 64
+        self.batch_size = 1
 
         self.use_cuda = torch.cuda.is_available()
 
